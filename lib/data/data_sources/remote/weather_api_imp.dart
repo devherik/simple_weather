fdd698290a1a00004430a47286fdcded0b2675a9
Herik Colares Rezende <dev.herik@gmail.com>
2024-10-12 10:12:42 -0300

    logic: getting the weather correctly

--- lib/data/data_sources/remote/weather_api_imp.dart
+++ lib/data/data_sources/remote/weather_api_imp.dart
@@ -1,3 +1,33 @@
 import 'package:simple_weather_app/infra/port/input/weather_api.dart';
+import 'package:weather/weather.dart';
 
-class WeatherApiImp implements WeatherApi {}
+class WeatherApiImp implements WeatherApi {
+  WeatherApiImp({required this.key});
+  @override
+  final String key;
+  WeatherFactory? factory;
+
+  @override
+  getWeatherByCity(String city) async {
+    final Weather weather = await factory!.currentWeatherByCityName(city);
+    return weather;
+  }
+
+  @override
+  getWeatherByLocation(double lat, double lon) async {
+    final Weather weather = await factory!.currentWeatherByLocation(lat, lon);
+    return weather;
+  }
+
+  @override
+  getForecastByCity(String city) async {
+    final List<Weather> forecast =
+        await factory!.fiveDayForecastByCityName(city);
+    return forecast;
+  }
+
+  @override
+  initAPI() {
+    factory = WeatherFactory(key);
+  }
+}
--- lib/features/home.dart
+++ lib/features/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:simple_weather_app/data/data_sources/remote/weather_api_imp.dart';
+import 'package:simple_weather_app/infra/port/input/weather_api.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +10,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String key = '41bdaa6c0895ca108bf29a888bdf885e';
+  late WeatherApi? _weatherAPI;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherAPI = WeatherApiImp(key: key);
+    _weatherAPI!.initAPI();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.all(8),
+          child: FutureBuilder(
+            future: _weatherAPI!.getWeatherByCity('Timóteo'),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Center(
+                  child: Text(snapshot.data.toString()),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
         ),
       ),
     );

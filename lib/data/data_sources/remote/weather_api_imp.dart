d1484b11ab88cad679b5f52cebbc8373443ad10e
Herik Colares Rezende <dev.herik@gmail.com>
2024-10-12 07:54:53 -0300

    struc: init the structure folders

---
+++ lib/data/data_sources/remote/weather_api_imp.dart
@@ -0,0 +1,3 @@
+import 'package:simple_weather_app/infra/port/input/weather_api.dart';
+
+class WeatherApiImp implements WeatherApi {}
---
+++ lib/infra/port/input/weather_api.dart
@@ -0,0 +1,5 @@
+abstract class WeatherApi {
+  /* this model defines the way a weather api will be handle for the app
+  if you change the api, the new one will work properly because it uses this
+  model as reference*/
+}
